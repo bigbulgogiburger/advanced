59dac40e42e3521bebe4b2b36e02e5d8d9283d2a
dohoon pyun <77317742+bigbulgogiburger@users.noreply.github.com>
2023-03-26 18:47:40 +0900

    added

---
+++ src/test/java/hello/advanced/trace/threadLocal/FieldServiceTest.java
@@ -0,0 +1,48 @@
+package hello.advanced.trace.threadLocal;
+
+import hello.advanced.trace.threadLocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest{
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field(){
+        log.info("main start");
+        Runnable userA= () ->{
+            fieldService.logic("userA");
+        };
+
+        log.info("main start");
+        Runnable userB= () ->{
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); //동시성 문제 발생 x
+        sleep(100); //동시성 문제 발생 o
+
+        threadB.start();
+
+        sleep(2000);
+        log.info("main exit");
+
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
